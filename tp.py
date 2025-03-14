0daf63d260a677d15e362187311ef14f3381462b
ferdaous <154676391+feerdaaaaous@users.noreply.github.com>
2025-03-14 23:33:54 +0100

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+def sigmoid(v):
+    return 1 / (1 + np.exp(-v))
+
+w1 = np.random.randn(3, 3) * 0.1 #multipe *0,1 pour eviter que le gradient devient proche de 0 (eviter les poids trop grands)
+b1=np.random.randn(3,1)*0.1 
+# w1 et b1 est les poids et biais (couche entree  3 neurones)
