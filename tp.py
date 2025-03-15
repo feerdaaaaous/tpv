3c93cc5b01ad2484dd57a7adf36a9055f5d103c2
ferdaous <154676391+feerdaaaaous@users.noreply.github.com>
2025-03-15 23:32:57 +0100

    Update tp.py

--- tp.py
+++ tp.py
@@ -2,12 +2,16 @@ import numpy as np
 
 class MLP:
     def __init__(self):
+        #multipe *0,1 pour eviter que le fonctions d'activation devient presque 0 (eviter les poids  trop grands ou trop petit)
         self.w1 = np.random.randn(3, 3) * 0.1
         self.b1 = np.random.randn(3, 1) * 0.1  
+        # w1 et b1 est les poids et biais (couche entree  3 neurones)
         self.w2 = np.random.randn(2, 3) * 0.1
         self.b2 = np.random.randn(2, 1) * 0.1
+        #entre la couch cachée1 (3neurones )et cachée2(2 neurones)
         self.w3 = np.random.randn(1, 2) * 0.1
         self.b3 = np.random.randn(1, 1) * 0.1
+        #entre la couche cachée2 et sortie ( 1 neurones )
     def afficher_parametres(self):
         print(f"w1 = \n{self.w1}\n")
         print(f"b1 = \n{self.b1}\n")
@@ -20,19 +24,20 @@ class MLP:
 
     def propa_vers_avant(self, vars):
         self.c1 = np.dot(self.w1, vars) + self.b1
-        self.s1 = self.sigmoid(self.c1)
+        self.s1 = self.sigmoid(self.c1)#matrice 3*1
         self.c2 = np.dot(self.w2, self.s1) + self.b2
-        self.s2 = self.sigmoid(self.c2)
+        self.s2 = self.sigmoid(self.c2)#matrice 2*1
         self.c3 = np.dot(self.w3, self.s2) + self.b3
         self.s = self.sigmoid(self.c3)
         
         return self.s 
     
-    def retropropagation(self):
-        s=
+    def retropropagation(self,vars):
+        sc=self.propa_vers_avant(vars)
+        
 reseau = MLP()
 reseau.afficher_parametres()
-vars = np.array([[0.5], [0.2], [-0.3]])
+vars = np.array([[0.5], [0.2], [-0.3]])#decalaration des variable (les entrées 3 )
 sortie_finale = reseau.propa_vers_avant(vars)
 print(f"sortie finale du réseau :\n{sortie_finale}")
 
